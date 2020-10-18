100455a9330d6ae49ba08a5bd841819a229a8bf9
Jubo-cade <jubomdivnishvili@gmail.com>
2020-10-18 16:57:46 +0400

    Updates

---
+++ precache-manifest.8d3347205ff6163d38080924fffe67b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b8fd9fb6490e807d4b83eb91642c5e",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "bb95ae6c6c2ee28f2823",
+    "url": "/todo-list/static/css/main.0d18d9a1.chunk.css"
+  },
+  {
+    "revision": "cc806112122a48a00f67",
+    "url": "/todo-list/static/js/2.9227f636.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-list/static/js/2.9227f636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb95ae6c6c2ee28f2823",
+    "url": "/todo-list/static/js/main.36daf8ed.chunk.js"
+  },
+  {
+    "revision": "7b15bc2f73099ab4ac07",
+    "url": "/todo-list/static/js/runtime-main.21a12556.js"
+  }
+]);
